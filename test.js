1f570c1da97877c4743fb689c3a1e59dbbe8939c
何健 <john.he@fairlinkcentury.com>
2017-03-31 20:37:46 +0800

    add test case

--- test.js
+++ test.js
@@ -26,7 +26,7 @@ import { createStore, combineReducers } from 'redux'
 
 var store_0 = createStore(userReducer)
 
-store_0.subscribe(function() {
+store_0.subscribe(function () {
     console.log('store_0 has been updated. Latest store state:', store_0.getState());
     // 在这里更新你的视图
 })
@@ -50,3 +50,36 @@ store_0.dispatch({
 console.log('store_0 state after action ADD_PERSON:', store_0.getState())
 
 console.log('store_0 state after action ADD_PERSON:', store_0.getState()["John"])
+
+class Todo {
+    constructor(text) {
+        this.id = Todo.nextTodoId++;
+        this.text = text;
+        this.completed = false;
+    }
+}
+
+Todo.nextTodoId = 0;
+
+var todo = new Todo('hell');
+console.log(new Todo());
+console.log(new Todo());
+
+class Todos {
+    constructor() {
+        this.state = [];
+    }
+
+    add(todo) {
+        this.state.push(todo);
+    }
+}
+
+var todos = new Todos();
+todos.add(new Todo());
+todos.add(new Todo());
+todos.add(new Todo());
+todos.add(new Todo());
+todos.add(new Todo());
+console.log(todos);
+
